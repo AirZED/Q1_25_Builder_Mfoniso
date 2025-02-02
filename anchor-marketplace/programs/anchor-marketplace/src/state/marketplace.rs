fb55a76da02be11c1e7ce918741db9d81c55a783
Mfoniso Ukpabio <regnalukpabio@gmail.com>
2025-02-02 06:14:32 +0100

    update

--- anchor-marketplace/programs/anchor-marketplace/src/state/marketplace.rs
+++ anchor-marketplace/programs/anchor-marketplace/src/state/marketplace.rs
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 pub struct MarketPlace {
     pub admin: Pubkey,     // 8 bytes
     pub fee: u16,          // 2 bytes
-    pub bump: u16,         // 2 byte
+    pub bump: u8,          // 2 byte
     pub treasury_bump: u8, // 1 byte
     pub reward_bump: u8,   // 1 byte
 
